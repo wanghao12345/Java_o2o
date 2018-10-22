f28c3ddb9b146072f30a5a371061fa3d2445a60e
winner <1750110604@qq.com>
2018-10-22 17:39:24 +0800

    logback

---
+++ o2o/src/main/java/com/wh/o2o/dao/AreaDao.java
@@ -0,0 +1,13 @@
+package com.wh.o2o.dao;
+
+import com.wh.o2o.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    /**
+     * 列出区域列表
+     * @return areaList
+     */
+    List<Area> queryArea();
+}
---
+++ o2o/src/main/java/com/wh/o2o/service/AreaService.java
@@ -0,0 +1,9 @@
+package com.wh.o2o.service;
+
+import com.wh.o2o.entity.Area;
+
+import java.util.List;
+
+public interface AreaService {
+    List<Area> getAreaList();
+}
