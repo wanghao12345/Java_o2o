1cddc94bf088f3a288e1aa630c406e74094b026f
winner <1750110604@qq.com>
2018-10-26 11:38:47 +0800

    update

--- o2o/src/main/java/com/wh/o2o/util/ImageUtil.java
+++ o2o/src/main/java/com/wh/o2o/util/ImageUtil.java
@@ -7,11 +7,17 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class ImageUtil {
     //项目水印图的基本路径
-
     private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    //时间格式
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    //随机数
+    private static final Random r = new Random();
     /**
      * 缩略图
      * @return
@@ -19,9 +25,9 @@ public class ImageUtil {
     public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr){
         //生成随机文件名
         String realFileName = getRandomFileName();
-        //生成文件版本
+        //获取文件后缀名
         String extension = getFileExtension(thumbnail);
-        //对应路径创建文件夹
+        //创建目标路径所涉及到的目录
         makeDirPath(targetAddr);
         //文件具体位置
         String relativeAddr = targetAddr + realFileName + extension;
@@ -40,30 +46,44 @@ public class ImageUtil {
     }
 
     /**
-     * 对应路径创建文件夹
-     * @param targetAddr
-     */
-    private static void makeDirPath(String targetAddr) {
-
-    }
-
-    /**
-     * 生成文件版本
-     * @param thumbnail
-     * @return
-     */
-    private static String getFileExtension(CommonsMultipartFile thumbnail) {
-        return null;
-    }
-
-    /**
-     * 生成随机文件名,当前年月日小时分钟秒钟
+     * 生成随机文件名,当前年月日小时分钟秒钟+五位随机数
      * @return
      */
     private static String getRandomFileName() {
-        return null;
+        //获取随机五位数
+        int rannum = r.nextInt(89999) + 10000;
+        //当前的时间
+        String nowTimeStr = sDateFormat.format(new Date());
+        return nowTimeStr + rannum;
     }
 
+    /**
+     * 获取输入文件流的扩展名
+     * @param cFile
+     * @return
+     */
+    private static String getFileExtension(CommonsMultipartFile cFile) {
+        //获取文件名
+        String originalFileName = cFile.getOriginalFilename();
+        //获取文件名的后缀
+        return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+    /**
+     * 创建目标路径所涉及到的目录，即/home/work/xiangze/xxx.jpg
+     * 那么home work xiangze这三个文件夹都得自动创建
+     * @param targetAddr
+     */
+    private static void makeDirPath(String targetAddr) {
+        //获取文件存放路径
+        String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+        //获取文件路径文件夹
+        File dirPath = new File(realFileParentPath);
+        //如果文件路径文件夹不存在创建
+        if (!dirPath.exists()){
+            dirPath.mkdirs();
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         Thumbnails.of(new File("C:/Users/wh/Desktop/img/timg.jpg")).size(200, 200)
